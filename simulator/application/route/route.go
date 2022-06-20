9944ad56f77ed690c7fe32b8f8592608cae22ffd
Thales Macêdo <thalesmacedo1@live.com>
2022-06-20 15:45:28 -0300

    created export json positions method to route

--- simulator/application/route/route.go
+++ simulator/application/route/route.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"os"
 	"strconv"
@@ -10,13 +11,20 @@ import (
 
 type Route struct {
 	ID        string
-	ClienteID string
+	ClientID  string
 	Positions []Position
 }
 
 type Position struct {
 	Lat  float64
 	Long float64
+}
+
+type PartialRoutePosition struct {
+	ID       string    `json:"routeId"`
+	ClientID string    `json:"clientId"`
+	Position []float64 `json:"position"`
+	Finished bool      `json:"finished"`
 }
 
 func (r *Route) LoadPositions() error {
@@ -48,4 +56,25 @@ func (r *Route) LoadPositions() error {
 		})
 	}
 	return nil
+}
+
+func (r *Route) ExportJsonPositions() ([]string, error) {
+	var route PartialRoutePosition
+	var result []string
+	total := len(r.Positions)
+	for k, v := range r.Positions {
+		route.ID = r.ID
+		route.ClientID = r.ClientID
+		route.Position = []float64{v.Lat, v.Long}
+		route.Finished = false
+		if total-1 == k {
+			route.Finished = true
+		}
+		jsonRoute, err := json.Marshal(route)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, string(jsonRoute))
+	}
+	return result, nil
 }
