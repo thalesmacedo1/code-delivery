088835e5d8102d8da35924aeff7ca5e3d4a94c5e
Thales Macêdo <thalesmacedo1@live.com>
2022-06-20 15:26:00 -0300

    created destinations folder and route method to get positions

---
+++ simulator/application/route/route.go
@@ -0,0 +1,51 @@
+package route
+
+import (
+	"bufio"
+	"errors"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Route struct {
+	ID        string
+	ClienteID string
+	Positions []Position
+}
+
+type Position struct {
+	Lat  float64
+	Long float64
+}
+
+func (r *Route) LoadPositions() error {
+	if r.ID == "" {
+		return errors.New("route id not informed")
+	}
+
+	f, err := os.Open("destinations" + r.ID + ".txt")
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		data := strings.Split(scanner.Text(), ",")
+		lat, err := strconv.ParseFloat(data[0], 64)
+		if err != nil {
+			return err
+		}
+		long, err := strconv.ParseFloat(data[1], 64)
+		if err != nil {
+			return err
+		}
+
+		r.Positions = append(r.Positions, Position{
+			Lat:  lat,
+			Long: long,
+		})
+	}
+	return nil
+}
---
+++ simulator/go.mod
@@ -0,0 +1,3 @@
+module github.com/thalesmacedo1/code-delivery/simulator
+
+go 1.16
